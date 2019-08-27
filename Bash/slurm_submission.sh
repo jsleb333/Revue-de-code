eb139440da9b8e1746d2632f1483ebc1b2b999dd
Guillaume St-Onge <guillaume.st-onge.4@ulaval.ca>
2019-08-27 17:36:26 -0400

    wip

--- Bash/slurm_submission.sh
+++ Bash/slurm_submission.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH --account=def-gstonge
+#SBATCH --account=def-ljdube
 #SBATCH --time=0-0:5
 #SBATCH --job-name=test
-#SBATCH --error=$HOME/projects/def-ljdube/gstonge/log/%x-%j.err
-#SBATCH --output=$HOME/projects/def-ljdube/gstonge/log/%x-%j.out
+#SBATCH --error=%x-%j.err
 #SBATCH --array=0-9
 
 module load python/3.7.4
@@ -12,7 +11,7 @@ module load scipy-stack/2019a
 param_array=($(seq 0 0.1 10))
 param=${param_array[$SLURM_ARRAY_TASK_ID]}
 
-outfile="$HOME/projects/def-ljdube/gstonge/data/file_$SLURM_ARRAY_TASK_ID.txt"
+outfile="file_$SLURM_ARRAY_TASK_ID.txt"
 
 echo $param > outfile
 
